998cb66c7a3ce297dafb20af89c941b9a67b4059
Vlad Kcorp <vladkcorp@gmail.com>
2021-08-13 19:22:33 +0300

    testing SHH connection

--- firstClass/Program.java
+++ firstClass/Program.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 import java.lang.Math.*;
+
 public class Program {
     public static void main(String[ ] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         double y = sc.nextInt();
-        
         Square a = new Square(x);
         Circle b = new Circle(y);
+
         a.area();
+
+        System.out.println("HELLO");
         b.area_circle();
 System.out.println("area of Square is " + a.area());
 System.out.println("area of Circle is " + b.area_circle());
