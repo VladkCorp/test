91869cde8d0a16a69d4e8622299e2cb27ae4bd47
Vlad Kcorp <vladkcorp@gmail.com>
2021-08-16 13:24:40 +0300

    Fixed discount percentage error
    Watch your commits! I just resolved a stupid merge conflict.

--- DiscountCalculationService.java
+++ DiscountCalculationService.java
@@ -1,15 +1,7 @@
 
 public class DiscountCalculationService {
+
     public static void main(String[] args) {
-        int discountPercentage = 0;
-        int sum = 0;
-
-
-        //TODO  bigger check = bigger discount, add it up
-        //check price policies in marketing ledger
-        if (sum < 1000) {
-            //okay
-    public static void main(String[] args){
         int discountPercentage = 0;
         int checkSum = 0;
 
