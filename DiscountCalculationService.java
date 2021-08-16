e218d4fd9fd86359a272d1795d0affc2d94d3b17
Vlad Kcorp <vladkcorp@gmail.com>
2021-08-16 09:56:19 +0300

    Added info what you should do to calcualte discount
    Will help you make discount calculations

--- DiscountCalculationService.java
+++ DiscountCalculationService.java
@@ -9,6 +9,14 @@ public class DiscountCalculationService {
         //check price policies in marketing ledger
         if (sum < 1000) {
             //okay
+    public static void main(String[] args){
+        int discountPercentage = 0;
+        int checkSum = 0;
+
+        //TODO  bigger check = bigger discount, add it up
+        //check price policies in marketing ledger
+        if (checkSum > 1000) {
+
         }
     }
 }
