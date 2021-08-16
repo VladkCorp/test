20ab9b6df0314988119467717fe2c26dafc30e97
Vlad Kcorp <vladkcorp@gmail.com>
2021-08-16 13:44:07 +0300

    New variable i and some spaces added

--- DiscountCalculationService.java
+++ DiscountCalculationService.java
@@ -1,9 +1,10 @@
-
+      
 public class DiscountCalculationService {
 
     public static void main(String[] args) {
         int discountPercentage = 0;
         int checkSum = 0;
+	int i = 0;
 
         //TODO  bigger check = bigger discount, add it up
         //check price policies in marketing ledger
