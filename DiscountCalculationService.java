26d0bb576b80be2ab1124349b300360bdfa940b8
Vlad Kcorp <vladkcorp@gmail.com>
2021-08-16 10:15:43 +0300

    Changed discount description and created logic template
    You can code now, guys

--- DiscountCalculationService.java
+++ DiscountCalculationService.java
@@ -1,13 +1,14 @@
 
 public class DiscountCalculationService {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int discountPercentage = 0;
-        int checkSum = 0;
+        int sum = 0;
+
 
         //TODO  bigger check = bigger discount, add it up
         //check price policies in marketing ledger
-        if (checkSum > 1000) {
-
+        if (sum < 1000) {
+            //okay
         }
     }
 }
